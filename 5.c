615c9df3624df7aacd2a9b9d1154f9c675ec848d
guest1 <guest1@guest1.(none)>
2014-09-12 20:58:53 +0800

    上传5.c

---
+++ 5.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 5.c
+	> User: Sunstars
+	> Mail:773662578@qq.com 
+	> Created Time: Fri 12 Sep 2014 08:42:57 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(int argc,char** argv)
+{
+
+hell
+
+
+
+
+
+
+
+
+
+printf();	
+	return 0;
+}
